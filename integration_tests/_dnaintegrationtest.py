bad7e4028568e80faa27ce3551b85a37f2002144
wilkos <wilko.steinhoff@dans.knaw.nl>
2016-10-11 09:15:35 +0200

    Changed admin email more generic one: harvester@dans.knaw.nl

--- integration_tests/_dnaintegrationtest.py
+++ integration_tests/_dnaintegrationtest.py
@@ -122,7 +122,7 @@ class IntegrationTest(SeecrTestCase):
         #print 'Identify:', body.xml()
         self.assertEquals('HTTP/1.0 200 OK\r\nContent-Type: text/xml; charset=utf-8', header)
         self.assertEquals('Gemeenschappelijke Harvester DANS-KB', body.OAI_PMH.Identify.repositoryName)
-        self.assertEquals('ishan.sital@dans.knaw.nl', body.OAI_PMH.Identify.adminEmail)
+        self.assertEquals('harvester@dans.knaw.nl', body.OAI_PMH.Identify.adminEmail)
 
     def testOaiListSets(self):
         header, body = getRequest(reactor, port, '/oai', {'verb': 'ListSets'})
