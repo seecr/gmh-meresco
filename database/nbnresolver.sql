14548df8983ff2b4fb5603b5f9a7a0798bda6467
wilkos-dans <wilko.steinhoff@dans.knaw.nl>
2021-09-08 15:12:16 +0200

    Use storedprocedures and credentials table in DB

--- database/nbnresolver.sql
+++ database/nbnresolver.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `nbnresolver` /*!40100 DEFAULT CHARACTER SET utf8
 USE `nbnresolver`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
--- Host: tgharvester31.dans.knaw.nl    Database: tst_nbnresolver
+-- Host: tgharvester31.dans.knaw.nl    Database: nbnresolver
 -- ------------------------------------------------------
 -- Server version	5.5.5-10.3.17-MariaDB
 
@@ -16,6 +16,25 @@ USE `nbnresolver`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `credentials`
+--
+
+DROP TABLE IF EXISTS `credentials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `credentials` (
+  `credentials_id` int(11) NOT NULL AUTO_INCREMENT,
+  `registrant_id` int(11) NOT NULL,
+  `username` varchar(150) DEFAULT NULL,
+  `password` varchar(150) DEFAULT NULL,
+  `token` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`credentials_id`),
+  KEY `FK_registrant_id_idx` (`registrant_id`),
+  CONSTRAINT `FK_registrant_id` FOREIGN KEY (`registrant_id`) REFERENCES `registrant` (`registrant_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `identifier`
@@ -113,10 +132,101 @@ CREATE TABLE `registrant` (
   `registrant_id` int(11) NOT NULL AUTO_INCREMENT,
   `registrant_groupid` varchar(255) NOT NULL,
   `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `isLTP` tinyint(1) NOT NULL DEFAULT '0',
+  `prefix` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`registrant_id`),
   UNIQUE KEY `registrant_groupid_UNIQUE` (`registrant_groupid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'tst_nbnresolver'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `deleteNbnLocationsByRegistrant` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`tst_nbnresolver`@`localhost` PROCEDURE `deleteNbnLocationsByRegistrant`(IN nbn_value VARCHAR(510), IN registrantid INT(11))
+BEGIN
+
+DECLARE identifierid BIGINT(20);
+DECLARE failover BOOLEAN;
+
+START TRANSACTION;
+
+	SELECT I.identifier_id INTO identifierid FROM identifier I WHERE I.identifier_value = nbn_value;
+    SELECT R.isLTP INTO failover FROM registrant R WHERE R.registrant_id = registrantid;
+
+-- Deze delete alle record uit IL en corresponderende records uit LR. Hij laat Locatie's en NBN staan in de betreffende tabellen.
+	DELETE IL, LR FROM identifier_location IL INNER JOIN location_registrant LR ON IL.location_id = LR.location_id WHERE LR.registrant_id = registrantid AND IL.identifier_id = identifierid AND IL.isFailover = failover;
+
+-- Cleanup locations table, but do not bother if some keys still exist in reference tables:
+	DELETE IGNORE l FROM location l  LEFT JOIN identifier_location IL ON IL.location_id = l.location_id WHERE IL.location_id IS NULL;
+
+COMMIT;
+END ;;
+DELIMITER ;
+
+--
+-- Dumping routines for database 'tst_nbnresolver'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `insertNbnLocation` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`tst_nbnresolver`@`localhost` PROCEDURE `insertNbnLocation`(IN nbn_value VARCHAR(510), IN nbn_location VARCHAR(1022), IN registrant_id INT(11) )
+BEGIN
+
+DECLARE loc_identifierid BIGINT(20);
+DECLARE loc_locationid BIGINT(20);
+DECLARE failover BOOLEAN;
+
+START TRANSACTION;
+
+	SELECT I.identifier_id INTO loc_identifierid from identifier I where I.identifier_value = nbn_value;
+	SELECT L.location_id INTO loc_locationid from location L where L.location_url = nbn_location;
+    SELECT R.isLTP INTO failover FROM registrant R WHERE R.registrant_id = registrant_id;
+
+
+	IF (loc_identifierid IS NULL) THEN
+		INSERT INTO identifier (identifier_value) VALUES (nbn_value);
+		SET loc_identifierid = LAST_INSERT_ID();
+	END IF;
+	INSERT IGNORE INTO identifier_registrant (identifier_id, registrant_id) VALUES (loc_identifierid, registrant_id );
+
+	IF (loc_locationid IS NULL) THEN
+		INSERT INTO location (location_url) VALUES (nbn_location);
+		SET loc_locationid = LAST_INSERT_ID();
+	END IF;
+	INSERT IGNORE INTO location_registrant (location_id, registrant_id) VALUES (loc_locationid, registrant_id );
+
+	IF (failover) THEN
+		SET @location = loc_locationid, @identifier = loc_identifierid, @last_update=NOW(), @failover=failover;
+		INSERT INTO identifier_location (location_id, identifier_id, last_modified, isFailover) VALUES (@location, @identifier, @last_update, @failover)
+		ON DUPLICATE KEY UPDATE location_id=@location, identifier_id=@identifier, last_modified=@last_update, isFailover=@failover;
+	ELSE
+		SET @location = loc_locationid, @identifier = loc_identifierid, @last_update=NOW(), @failover=failover;
+		INSERT INTO identifier_location (location_id, identifier_id, last_modified, isFailover) VALUES (@location, @identifier, @last_update, @failover)
+		ON DUPLICATE KEY UPDATE location_id=@location, identifier_id=@identifier, last_modified=@last_update;
+	END IF;
+
+COMMIT;
+END ;;
+DELIMITER ;
+
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
